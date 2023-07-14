ccd454f4933300c192e210d622cf1b98d7d066e2
Alexander <okyxxxf@gmail.com>
2023-07-15 00:38:51 +0300

    feat: make form and add item component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,21 @@
+import '../css/Form.css';
+import '../css/Input.css';
+
 export default function Form() {
     return (
         <div className="to-do__form">
-            
+            <AddItem />
         </div>
     );
 }
+
+function AddItem() {
+    return (
+        <div className="add-item">
+            <div className="add-item__label">add item</div>
+            <input className="add-item__input input" type="text" placeholder="Add task" />
+            <span className="add-item__add-button">Add</span>
+        </div>
+    );
+}
+
